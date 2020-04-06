58db07991fa4637c7280104eba526e17b9c09dcd
fabio <fabio.aoki.93@gmail.com>
2020-04-06 18:40:36 -0300

    criando endpoint POST

--- src/main/java/com/aoki/forecast/controller/ForecastController.java
+++ src/main/java/com/aoki/forecast/controller/ForecastController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,15 @@ public class ForecastController {
 		SeasonDto seasonDto = forecastService.verify(id);
 		if(Objects.nonNull(seasonDto)) {
 			return new ResponseEntity<SeasonDto>(seasonDto,HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@RequestMapping(value="/weather",method = RequestMethod	.POST)
+	public ResponseEntity<SeasonDto> insertCity(@RequestBody String name, String city) {
+		SeasonDto insertCity = forecastService.insertCity(name, city);
+		if(Objects.nonNull(insertCity)) {
+			return new ResponseEntity<SeasonDto>(insertCity,HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
--- src/main/java/com/aoki/forecast/service/ForecastService.java
+++ src/main/java/com/aoki/forecast/service/ForecastService.java
@@ -2,17 +2,27 @@ package com.aoki.forecast.service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
 
 import com.aoki.forecast.dto.SeasonDto;
 
+
 @Service
 public class ForecastService {
-
+	
+	private static long contador = 1;
+	
+	public void insertCityCreateId() {
+		SeasonDto.builder().id(+contador);
+		contador++;
+	}
+	
 	public SeasonDto verify(long id) {
 
+		
 		if(id == 1) {
 			Date localData = formatDate("10-01-2020");
 			return SeasonDto.builder().id(id).name("sun").city("São Paulo").
@@ -37,4 +47,28 @@ public class ForecastService {
 		}
 		return localData;
 	}
+
+	public SeasonDto insertCity(String name, String city) {
+		try {
+			insertCityCreateId();
+			Date localData = formatDate("10-01-2020");
+			
+			//ainda nai sendo utilizado
+//			ArrayList<SeasonDto> cityInformation = new ArrayList<SeasonDto>();
+//			extracted(name, city, localData, cityInformation);
+//			
+			
+			return SeasonDto.builder().id(contador).name(name).city(city).
+					dateCity(localData).build();
+		} catch (IllegalArgumentException e) {
+			System.out.println("Campo invalido");
+		}
+		return null;
+	}
+
+	private boolean extracted(String name, String city, Date localData, ArrayList<SeasonDto> cityInformation) {
+		return cityInformation.add(SeasonDto.builder().id(contador).name(name).city(city).
+				dateCity(localData).build());
+	}
+
 }
